96b2f4cf92de658c0f3842dc3bdbed80ff5c162d
harmeet27 <harmeetkaur2793@gmail.com>
2020-04-30 01:58:51 +0530

    Chore: Initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render(){
+      return <div>My App Setup</div>
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+const { HOST='0.0.0.0', PORT = 8080 } = process.env;
+
+module.exports = () => {
+  const modulesFolderPath = path.resolve(__dirname, 'node_modules');
+  return{
+    entry: './src/index.js',
+    output:{
+      path: path.resolve(__dirname, 'build'),
+      filename: 'static/js/[name].[hash:8].js',
+      publicPath: '/'
+    },
+    resolve:{
+        modules: [modulesFolderPath, 'node_modules'],
+        extensions: ['.js', '.jsx'],
+        alias:{
+            components: path.resolve(__dirname, 'src/components')
+        }
+    },
+    module: {
+        rules: [
+          {
+            test: /(\.jsx|\.js)$/,
+            exclude: /node_modules/,
+            include: [
+                path.resolve(__dirname, 'src')
+            ],
+            use: [
+              'babel-loader'
+            ]
+          },
+          {
+              test: /\.css$/,
+              include: [
+                path.resolve(__dirname, 'src')
+              ],
+              use:['style-loader', 'css-loder']
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader"
+              }
+            ]
+          }
+        ]
+      },
+      devServer: {
+        host: HOST,
+        port: PORT,
+        public: `localhost:${PORT}`,
+        open: true,
+        headers: {'Access-Control-Allow-Origin': '*'}
+      },
+      plugins: [
+        new ProgressBarPlugin({
+            clear: false
+        }),
+        new HtmlWebPackPlugin({
+          cache: true,
+          template: "./src/static/index.html",
+          filename: "./index.html"
+        })
+      ]
+  }
+};
